8fe5b3880e913da8091d72fda002137bf760c104
Chris Fraley <cfraley@tableau.com>
2016-12-24 10:23:58 +0000

    version 0.15

--- R/getHDmembers.R
+++ R/getHDmembers.R
@@ -1,43 +1,46 @@
 getHDmembers <-
-function( data, maxrows = 10000, radius=NULL) 
+function (data, maxrows = 10000, radius = NULL) 
 {
-
- data <- as.matrix(data)
-
- n <- nrow(data)
- p <- ncol(data)
- 
-if (is.null(radius)) radius <- .1/(log(n)^(1/p))
-
-# nearest-neighbor package
-# require(FNN)
-  
- members <- rep(list(NULL),n)
-
- if (n <= maxrows) {
-   members <- as.list(1:n)
- }
- else {
-   exemplars <- 1
-   members[[1]] <- 1
-   for (i in 2:n) {
-      KNN <- get.knnx(data=data[exemplars,,drop=F],query=data[i,,drop=F],k=1)
-      m <- KNN$nn.index[1,1]
-      d <- KNN$nn.dist[1,1]
-      if (d < radius) {
-        l <- exemplars[m]
-        members[[l]] <- c(members[[l]],i)
-        next
+    data <- as.matrix(data)
+    n <- nrow(data)
+    p <- ncol(data)
+    if (is.null(radius)) 
+        radius <- 0.1/(log(n)^(1/p))
+    if (n <= maxrows) {
+      cl <- partuniq(data)  # requires mclust
+      U <- unique(cl)
+      m <- length(U)
+      if (m != n) {
+        members <- rep(list(NULL), m)
+        j <- 0
+        for (u in U) {
+             j <- j + 1
+             members[[j]] <- which(cl == u)
+        }
+#       names(members) <- U
       }
-      exemplars <- c(exemplars,i)
-      members[[i]] <- i
-   }
- }
-
-#names(members) <- 1:n
-members <- members[!sapply( members, is.null)]
-exemplars <- sapply( members, function(x) x[[1]])
-names(members) <- exemplars
-
-members
+      else members <- as.list(1:n)
+    }
+    else {
+        members <- rep(list(NULL), n)
+        exemplars <- 1
+        members[[1]] <- 1
+        for (i in 2:n) {
+            KNN <- get.knnx(data = data[exemplars, , drop = F], 
+                query = data[i, , drop = F], k = 1)
+            m <- KNN$nn.index[1, 1]
+            d <- KNN$nn.dist[1, 1]
+            if (d < radius) {
+                l <- exemplars[m]
+                members[[l]] <- c(members[[l]], i)
+                next
+            }
+            exemplars <- c(exemplars, i)
+            members[[i]] <- i
+        }
+    }
+    members <- members[!sapply(members, is.null)]
+    exemplars <- sapply(members, function(x) x[[1]])
+    names(members) <- exemplars
+    members
 }
--- R/getHDoutliers.R
+++ R/getHDoutliers.R
@@ -1,42 +1,31 @@
 getHDoutliers <-
-function( data, memberLists, alpha=.05)
+function (data, memberLists, alpha = 0.05) 
 {
-# require(FNN)
-    
- exemplars <- sapply( memberLists, function(x) x[[1]])
-
- data <- as.matrix(data)
-
- d <- knn.dist( data[exemplars,], k=1)
- n <- length(d)
- ord <- order(d)
- dmin <- min(d)
- dmax <- max(d)
-
- gaps <- c(0,diff(d[ord]))
-# names(gaps) <- 1:length(gaps)
-
- n4 <- max(min( 50, floor(n/4)),2)
- J <- 1:n4
-
- start <- max(floor(n/2),1)+1
- ghat <- numeric(n)
- for (i in start:n) ghat[i] <- sum((J/n4)*gaps[i-J+1])
-
- logAlpha <- log(1/alpha)
-
- use <- start:n
-
- bound <- Inf
- for (i in start:n) {
-    if (gaps[i] > logAlpha * ghat[i]) {
-      bound <- d[ord][i-1]
-      break
+    exemplars <- sapply(memberLists, function(x) x[[1]])
+    data <- as.matrix(data)
+    d <- knn.dist(data[exemplars, ], k = 1)
+    n <- length(d)
+    ord <- order(d)
+    dmin <- min(d)
+    dmax <- max(d)
+    gaps <- c(0, diff(d[ord]))
+    n4 <- max(min(50, floor(n/4)), 2)
+    J <- 1:n4
+    start <- max(floor(n/2), 1) + 1
+    ghat <- numeric(n)
+    for (i in start:n) ghat[i] <- sum((J/n4) * gaps[i - J + 1])
+    logAlpha <- log(1/alpha)
+    use <- start:n
+    bound <- Inf
+    for (i in start:n) {
+        if (gaps[i] > logAlpha * ghat[i]) {
+            bound <- d[ord][i - 1]
+            break
+        }
     }
- }
-
- ex <- exemplars[which(d > bound)]
- mem1 <- sapply( memberLists, function(x) x[1])
- out <- unlist(memberLists[match(ex,mem1)])
- out
+    ex <- exemplars[which(d > bound)]
+    mem1 <- sapply(memberLists, function(x) x[1])
+    out <- unlist(memberLists[match(ex, mem1)])
+    names(out) <- NULL
+    out
 }
