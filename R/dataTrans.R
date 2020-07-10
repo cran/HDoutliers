1d8f94224bfef46405f23f54b41ba1c84e0b69fc
Chris Fraley <cfraley@tableau.com>
2020-07-10 04:20:02 +0000

    version 1.0.2

--- R/dataTrans.R
+++ R/dataTrans.R
@@ -1,31 +1,38 @@
 dataTrans <-
-function(data) 
+function (data) 
 {
- if (any(is.na(data))) stop("missing values not allowed")
-
-# look for categorical variables
- if (is.null(dim(data))) {
-   CAT <- !is.numeric(data) 
-   data <- if (CAT) as.data.frame(data) else as.matrix(data)
- }
- else {
-   if (inherits( data, "matrix")) data <- as.data.frame(data)
-   CAT <- sapply( data, function(x) !is.numeric(x))
- }
-
- if (any(CAT)) {
-# convert each categorical variable to numeric via 
-# multiple correspondence analysis using MCA function from FactoMineR
-#  require(FactoMineR)
-   data[,CAT] <- sapply( data[,CAT,drop=F], 
-                         function(x) MCA(as.matrix(x),ncp=1,graph=F)$ind$coord)
- }
-
-unitize <- function(z) {
-               zrange <- range(z)
-               if (!(dif <- diff(zrange))) return(rep(0,length(z)))
-               (z - zrange[1])/dif
-                }
-
-apply( as.matrix(data), 2, unitize)
+    if (any(is.na(data))) 
+        stop("missing values not allowed")
+    if (is.null(dim(data))) {
+        CAT <- !is.numeric(data)
+        data <- if (CAT) 
+            as.data.frame(data)
+        else as.matrix(data)
+    }
+    else {
+        if (inherits(data, "matrix")) 
+            data <- as.data.frame(data)
+        CAT <- sapply(data, function(x) !is.numeric(x))
+    }
+# look for constant variables 
+    cte <- apply( data, 2, function(x) length(unique(x)) == 1)
+    if (all(cte)) {
+      warning("all columns have a constant values")
+      return(NULL)
+    }
+    else if (any(cte)) warning("some columns have a constant values")
+    data <- data[,!cte,drop=F]
+    CAT <- CAT[!cte]
+    if (any(CAT)) {
+        data[, CAT] <- sapply(data[, CAT, drop = F], function(x) 
+             MCA(as.matrix(x), ncp = 1, graph = F)$ind$coord)
+    }
+    unitize <- function(z) {
+        zrange <- range(z)
+        if (!(dif <- diff(zrange))) 
+            return(rep(0, length(z)))
+        (z - zrange[1])/dif
+    }
+    print(dim(data))
+    apply(as.matrix(data), 2, unitize)
 }
